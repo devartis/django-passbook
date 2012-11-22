a5267d5035f8e2ffb6fa8dea1d45f4cc75b82b82
Fernando Aramendi <fernando@devartis.com>
2012-11-22 13:26:27 -0300

    fixed bad fk

--- django_passbook/models.py
+++ django_passbook/models.py
@@ -5,7 +5,6 @@ class Pass(models.Model):
     pass_type_identifier = models.CharField(max_length=50)
     serial_number = models.CharField(max_length=50)
     authentication_token = models.CharField(max_length=50)
-    store = models.ForeignKey(Store)
 
     def __unicode__(self):
         return self.serial_number
@@ -14,7 +13,7 @@ class Pass(models.Model):
 class Registration(models.Model):
     device_library_identifier = models.CharField(max_length=50)
     push_token = models.CharField(max_length=50)
-    pass_ = models.ForeignKey(Registration)
+    pass_ = models.ForeignKey(Pass)
 
     def __unicode__(self):
         return self.device_library_identifier

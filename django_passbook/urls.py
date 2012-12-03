e83b000bf6c7a452d1b39025ee51bdd1e404eea9
Fernando Aramendi <fernando@devartis.com>
2012-12-03 11:02:29 -0300

    fixed import

--- django_passbook/urls.py
+++ django_passbook/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from django_passbook.views import register_pass
+from django_passbook.views import register_pass, latest_version
 
 urlpatterns = patterns('',
     url(r'^v1/devices/(?P<device_library_id>.+)/registrations/(?P<pass_type_id>[\w\.\d]+)/(?P<serial_number>.+)$', register_pass),

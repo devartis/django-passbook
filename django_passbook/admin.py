ccaad622da21ac0bac07379cd9843f131aa2ed97
Fernando Aramendi <fernando@devartis.com>
2012-12-05 14:23:48 -0300

    added logs to admin

--- django_passbook/admin.py
+++ django_passbook/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django_passbook.models import Pass, Registration
+from django_passbook.models import Pass, Registration, Log
 
 admin.site.register(Pass)
 admin.site.register(Registration)
+admin.site.register(Log)
